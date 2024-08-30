fb47972664e519faf0b48dd115eec8bdd2edfb99
sasikiran0 <sasikran@bloomitsolution.in>
2024-08-30 18:56:20 +0530

    keywords and variables

---
+++ script.js
@@ -0,0 +1,45 @@
+// keywords
+// var, let and const
+
+// var firstName;    // variable declaration
+// var lastName;
+// firstName = "John"  //value assignment
+// lastName = "Doe"
+
+// console.log(firstName + lastName);
+
+// firstName = "Tony"   //re-assignment
+// console.log(firstName)
+
+// var firstName = "Thor"  //re-declaration
+// console.log(firstName)
+
+// let firstName;
+// firstName = "Thor";
+// console.log(firstName);
+
+// firstName = "Hulk"; //re-assignment
+// console.log(firstName);
+
+// hoisting
+
+// console.log(myVar);
+// var myVar = "Hello";
+// temporal deadzone
+
+// var --> global scope
+// let and const --> block scope
+
+// {
+//   const x = 10;
+//   console.log(x);
+// }
+
+// console.log(x)
+
+{
+  var x = 10;
+  console.log(x);
+}
+
+console.log(x);
