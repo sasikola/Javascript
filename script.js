9241a43fe81b323bf8f2668890a75f1e071dadaa
sasikiran0 <sasikran@bloomitsolution.in>
2024-08-31 19:25:52 +0530

    operators

--- script.js
+++ script.js
@@ -46,18 +46,63 @@
 
 // data types
 // primitive
-// non-primitive
 
 // string --> "10", '', ``
 // number --> 98.5
 // boolean --> true or false
-// null --> object
+// null --> object (null)
 // undefined  -->
 
 // let x;
 // console.log(x);
 
+// non-primitive
 // object
 // functions
 // arrays
 
+// default value of a declared variable
+// synchronous programming language
+// key: value
+// let person = {
+//   name: "John",
+//   age: 30,
+//   city: "New York",
+// };
+// json --> javascript object notation
+
+// arrays
+// let x = [1, 2, 3, 4, 5, 4, 4, 6, , 7, 3, 45, 43, 46];
+// index value starting from 0
+// 1 - 0;
+// 2 - 1
+// let lastItem = x[x.length - 1];
+// console.log(lastItem);
+
+// javascript operators
+// operators are used to perform some operation on operands
+// arithmetic operator (+, -, *, /, %,)
+// unary operator (++, --)
+// assignment operator
+// comparison operator
+// logical operator
+
+// ++ increment (pre and post)
+// -- decrement (pre and post)
+
+// console.log(x++);//x = x+1
+// x++
+// console.log(x)
+// x++
+// console.log(x)
+// console.log(x)
+// x = 20
+// console.log(x)
+// let sum = x + y;
+// console.log(sum)
+// let sum1 = sum + x;
+// console.log(sum1)
+// let x = 10;
+// let y = 20;
+// // x - 1  --> 9
+// console.log(x-- + ++x)  //9 + (10+1)10 
